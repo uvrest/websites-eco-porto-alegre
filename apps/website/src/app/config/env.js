d218137bcffb104243079eba8545c8e2d14d0b4f
Uverest <mailtogabrielcoelho@gmail.com>
2025-11-07 11:53:46 -0300

    configs

--- apps/website/src/app/config/env.js
+++ apps/website/src/app/config/env.js
@@ -2,7 +2,7 @@ import { cleanEnv, url, bool, str } from "envalid";
 
 export const env = cleanEnv(import.meta.env, {
     VITE_APP_NAME: str({ default: "Eco Porto Alegre" }),
-    VITE_API_URL: url({ default: "http://localhost:3000" }),
+    VITE_API_URL: url({ default: "http://localhost:8000" }),
     VITE_BASE_PATH: str({ default: "/" }), // basename e base do Vite
     VITE_ENABLE_OBSERVABILITY: bool({ default: false }),
 }, { strict: false });
